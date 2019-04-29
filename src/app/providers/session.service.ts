4e174a215ddf624a52d78de3d8799af204f20f4d
GabrielRubio <gabriel.rubio.usp@gmail.com>
2019-04-29 14:17:58 -0300

    ajustando layouts

--- src/app/providers/session.service.ts
+++ src/app/providers/session.service.ts
@@ -11,10 +11,12 @@ export class SessionService {
 
   constructor(public storageLocal:StorageService) { }
 
+  // get a unique identifier id of user 
   getUserId(){
     return this.storageLocal.get('userId');
   }
 
+  // register data into the localStorage
   register(){
     if(!this.getUserId()){
       this.userId = this.generator();
@@ -22,13 +24,12 @@ export class SessionService {
     }
   }
 
-  // functions to creat a unique id
+  // functions to create a unique id
   private generator(): string {
     const isString = `${this.S4()}${this.S4()}-${this.S4()}-${this.S4()}-${this.S4()}-${this.S4()}${this.S4()}${this.S4()}`;
 
     return isString;
   }
-
   private S4(): string {
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
   }
