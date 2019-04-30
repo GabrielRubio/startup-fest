6579604fc6fa80dfd6020b8f15f2860710d75f07
GabrielRubio <gabriel.rubio.usp@gmail.com>
2019-04-29 22:15:56 -0300

    retirando firebase  api key

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,3 @@
 export const environment = {
-  production: true,
-  firebaseConfig: {
-    apiKey: "AIzaSyABa_KWyYdKBrLDBjBc9lA0FIINB_uxZAM",
-    authDomain: "startups-fest-wepapp.firebaseapp.com",
-    databaseURL: "https://startups-fest-wepapp.firebaseio.com",
-    projectId: "startups-fest-wepapp",
-    storageBucket: "startups-fest-wepapp.appspot.com",
-    messagingSenderId: "373121495225"
-  }
+  production: true
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,14 +4,6 @@
 
 export const environment = {
   production: false,
-  firebaseConfig: {
-    apiKey: "AIzaSyABa_KWyYdKBrLDBjBc9lA0FIINB_uxZAM",
-    authDomain: "startups-fest-wepapp.firebaseapp.com",
-    databaseURL: "https://startups-fest-wepapp.firebaseio.com",
-    projectId: "startups-fest-wepapp",
-    storageBucket: "startups-fest-wepapp.appspot.com",
-    messagingSenderId: "373121495225"
-  }
 };
 
 /*
