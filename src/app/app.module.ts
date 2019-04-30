c65e8f0ab7aba0039e96dbec8b96cc71d330d87e
GabrielRubio <gabriel.rubio.usp@gmail.com>
2019-04-29 21:38:45 -0300

    Arumando readme e imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,33 +1,33 @@
+import { environment } from '../environments/environment';
+
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-// modulos
-import { AppRoutingModule } from './app-routing.module';
-import { GraphQLModule } from './graphql.module';
 import { HttpClientModule } from '@angular/common/http';
 import { AngularFireModule } from '@angular/fire';
 import { AngularFirestoreModule, FirestoreSettingsToken } from '@angular/fire/firestore';
 import { AngularFireDatabaseModule } from '@angular/fire/database';
-
-// services
-import { StartupsService } from './providers/startups.service';
-
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { LayoutModule } from '@angular/cdk/layout';
 import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatCardModule, MatGridListModule } from '@angular/material';
 import { ServiceWorkerModule } from '@angular/service-worker';
-import { environment } from '../environments/environment';
-import { HeaderComponent } from './components/header/header.component';
+import { AppRoutingModule } from './app-routing.module';
+import { GraphQLModule } from './graphql.module';
+import { BarRatingModule } from "ngx-bar-rating";
+
+// services
+import { StartupsService } from './providers/startups.service';
+
+// Components
+import { AppComponent } from './app.component';
 import { HomePageComponent } from './pages/home-page/home-page.component';
 import { ResultsPageComponent } from './pages/results-page/results-page.component';
 import { StartupPageComponent } from './pages/startup-page/startup-page.component';
 import { ErrorPageComponent } from './pages/error-page/error-page.component';
+import { HeaderComponent } from './components/header/header.component';
 import { StartupCardComponent } from './components/startup-card/startup-card.component';
 import { StartupDetailsComponent } from './components/startup-details/startup-details.component';
 import { StartupListComponent } from './components/startup-list/startup-list.component';
-import { from } from 'rxjs';
-import { BarRatingModule } from "ngx-bar-rating";
 import { StarReviewComponent } from './components/star-review/star-review.component';
 import { RankingComponent } from './components/ranking/ranking.component';
 import { RankingItemComponent } from './components/ranking-item/ranking-item.component';
